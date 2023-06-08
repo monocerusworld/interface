8248d14d10733008358bae307b8b5f7f19d55480
Sofrosine <soultanmuh@gmail.com>
2023-06-08 07:53:38 +0700

    fix background color changes

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,25 +1,32 @@
-import { Trans } from '@lingui/macro'
-import { Trace, TraceEvent } from '@uniswap/analytics'
-import { BrowserEvent, InterfaceElementName, InterfacePageName, SharedEventName } from '@uniswap/analytics-events'
-import { AboutFooter } from 'components/About/AboutFooter'
-import Card, { CardType } from 'components/About/Card'
-import { MAIN_CARDS, MORE_CARDS } from 'components/About/constants'
-import ProtocolBanner from 'components/About/ProtocolBanner'
-import { BaseButton } from 'components/Button'
-import { useSwapWidgetEnabled } from 'featureFlags/flags/swapWidget'
-import { useAtomValue } from 'jotai/utils'
-import Swap from 'pages/Swap'
-import { parse } from 'qs'
-import { useEffect, useRef, useState } from 'react'
-import { ArrowDownCircle } from 'react-feather'
-import { useLocation, useNavigate } from 'react-router-dom'
-import { Link as NativeLink } from 'react-router-dom'
-import { shouldDisableNFTRoutesAtom } from 'state/application/atoms'
-import { useAppSelector } from 'state/hooks'
-import styled, { css } from 'styled-components/macro'
-import { BREAKPOINTS } from 'theme'
-import { useIsDarkMode } from 'theme/components/ThemeToggle'
-import { Z_INDEX } from 'theme/zIndex'
+import { Trans } from "@lingui/macro";
+import { Trace, TraceEvent } from "@uniswap/analytics";
+import {
+  BrowserEvent,
+  InterfaceElementName,
+  InterfacePageName,
+  SharedEventName,
+} from "@uniswap/analytics-events";
+import { useWeb3React } from "@web3-react/core";
+import { AboutFooter } from "components/About/AboutFooter";
+import Card, { CardType } from "components/About/Card";
+import { MAIN_CARDS, MORE_CARDS } from "components/About/constants";
+import ProtocolBanner from "components/About/ProtocolBanner";
+import { BaseButton } from "components/Button";
+import { getChainInfoOrDefault } from "constants/chainInfo";
+import { useSwapWidgetEnabled } from "featureFlags/flags/swapWidget";
+import { useAtomValue } from "jotai/utils";
+import Swap from "pages/Swap";
+import { parse } from "qs";
+import { useEffect, useRef, useState } from "react";
+import { ArrowDownCircle } from "react-feather";
+import { useLocation, useNavigate } from "react-router-dom";
+import { Link as NativeLink } from "react-router-dom";
+import { shouldDisableNFTRoutesAtom } from "state/application/atoms";
+import { useAppSelector } from "state/hooks";
+import styled, { css } from "styled-components/macro";
+import { BREAKPOINTS } from "theme";
+import { useIsDarkMode } from "theme/components/ThemeToggle";
+import { Z_INDEX } from "theme/zIndex";
 
 const PageContainer = styled.div<{ isDarkMode: boolean }>`
   position: absolute;
@@ -34,9 +41,9 @@ const PageContainer = styled.div<{ isDarkMode: boolean }>`
 
   background: ${({ isDarkMode }) =>
     isDarkMode
-      ? 'linear-gradient(rgba(8, 10, 24, 0) 0%, rgb(8 10 24 / 100%) 45%)'
-      : 'linear-gradient(rgba(255, 255, 255, 0) 0%, rgb(255 255 255 /100%) 45%)'};
-`
+      ? "linear-gradient(rgba(8, 10, 24, 0) 0%, rgb(8 10 24 / 100%) 45%)"
+      : "linear-gradient(rgba(255, 255, 255, 0) 0%, rgb(255 255 255 /100%) 45%)"};
+`;
 
 const Gradient = styled.div<{ isDarkMode: boolean }>`
   position: absolute;
@@ -49,15 +56,15 @@ const Gradient = styled.div<{ isDarkMode: boolean }>`
   min-height: 550px;
   background: ${({ isDarkMode }) =>
     isDarkMode
-      ? 'linear-gradient(rgba(8, 10, 24, 0) 0%, rgb(8 10 24 / 100%) 45%)'
-      : 'linear-gradient(rgba(255, 255, 255, 0) 0%, rgb(255 255 255 /100%) 45%)'};
+      ? "linear-gradient(rgba(8, 10, 24, 0) 0%, rgb(8 10 24 / 100%) 45%)"
+      : "linear-gradient(rgba(255, 255, 255, 0) 0%, rgb(255 255 255 /100%) 45%)"};
   z-index: ${Z_INDEX.under_dropdown};
   pointer-events: none;
   height: ${({ theme }) => `calc(100vh - ${theme.mobileBottomBarHeight}px)`};
   @media screen and (min-width: ${({ theme }) => theme.breakpoint.md}px) {
     height: 100vh;
   }
-`
+`;
 
 const GlowContainer = styled.div`
   position: absolute;
@@ -72,19 +79,23 @@ const GlowContainer = styled.div`
   @media screen and (min-width: ${({ theme }) => theme.breakpoint.md}px) {
     height: 100vh;
   }
-`
+`;
 
-const Glow = styled.div`
+const Glow = styled.div<{ bgColor: string }>`
   position: absolute;
   top: 68px;
   bottom: 0;
-  background: radial-gradient(72.04% 72.04% at 50% 3.99%, #ff37eb 0%, rgba(166, 151, 255, 0) 100%);
+  background: ${({ bgColor }) =>
+    `radial-gradient(
+    72.04% 72.04% at 50% 3.99%,
+    ${bgColor} 0%,
+    rgba(166, 151, 255, 0) 100%)`};
   filter: blur(72px);
   border-radius: 24px;
   max-width: 480px;
   width: 100%;
   height: 100%;
-`
+`;
 
 const ContentContainer = styled.div<{ isDarkMode: boolean }>`
   position: absolute;
@@ -97,13 +108,15 @@ const ContentContainer = styled.div<{ isDarkMode: boolean }>`
   max-width: min(720px, 90%);
   min-height: 500px;
   z-index: ${Z_INDEX.under_dropdown};
-  transition: ${({ theme }) => `${theme.transition.duration.medium} ${theme.transition.timing.ease} opacity`};
-  height: ${({ theme }) => `calc(100vh - ${theme.navHeight + theme.mobileBottomBarHeight}px)`};
+  transition: ${({ theme }) =>
+    `${theme.transition.duration.medium} ${theme.transition.timing.ease} opacity`};
+  height: ${({ theme }) =>
+    `calc(100vh - ${theme.navHeight + theme.mobileBottomBarHeight}px)`};
   pointer-events: none;
   * {
     pointer-events: auto;
   }
-`
+`;
 
 const TitleText = styled.h1<{ isDarkMode: boolean }>`
   color: transparent;
@@ -114,8 +127,8 @@ const TitleText = styled.h1<{ isDarkMode: boolean }>`
   margin: 0 0 24px;
   background: ${({ isDarkMode }) =>
     isDarkMode
-      ? 'linear-gradient(20deg, rgba(255, 244, 207, 1) 10%, rgba(255, 87, 218, 1) 100%)'
-      : 'linear-gradient(10deg, rgba(255,79,184,1) 0%, rgba(255,159,251,1) 100%)'};
+      ? "linear-gradient(20deg, rgba(255, 244, 207, 1) 10%, rgba(255, 87, 218, 1) 100%)"
+      : "linear-gradient(10deg, rgba(255,79,184,1) 0%, rgba(255,159,251,1) 100%)"};
   background-clip: text;
   -webkit-background-clip: text;
 
@@ -128,7 +141,7 @@ const TitleText = styled.h1<{ isDarkMode: boolean }>`
     font-size: 64px;
     line-height: 72px;
   }
-`
+`;
 
 const SubText = styled.div`
   color: ${({ theme }) => theme.textSecondary};
@@ -143,28 +156,29 @@ const SubText = styled.div`
     font-size: 20px;
     line-height: 28px;
   }
-`
+`;
 
 const SubTextContainer = styled.div`
   display: flex;
   justify-content: center;
-`
+`;
 
 const LandingButton = styled(BaseButton)`
   padding: 16px 0px;
   border-radius: 24px;
-`
+`;
 
 const ButtonCTA = styled(LandingButton)`
   background: linear-gradient(93.06deg, #ff00c7 2.66%, #ff9ffb 98.99%);
   border: none;
   color: ${({ theme }) => theme.white};
-  transition: ${({ theme }) => `all ${theme.transition.duration.medium} ${theme.transition.timing.ease}`};
+  transition: ${({ theme }) =>
+    `all ${theme.transition.duration.medium} ${theme.transition.timing.ease}`};
 
   &:hover {
     box-shadow: 0px 0px 16px 0px #ff00c7;
   }
-`
+`;
 
 const ButtonCTAText = styled.p`
   margin: 0px;
@@ -175,13 +189,13 @@ const ButtonCTAText = styled.p`
   @media screen and (min-width: ${BREAKPOINTS.sm}px) {
     font-size: 20px;
   }
-`
+`;
 
 const ActionsContainer = styled.span`
   max-width: 300px;
   width: 100%;
   pointer-events: auto;
-`
+`;
 
 const LearnMoreContainer = styled.div`
   align-items: center;
@@ -197,17 +211,18 @@ const LearnMoreContainer = styled.div`
     visibility: visible;
   }
 
-  transition: ${({ theme }) => `${theme.transition.duration.medium} ${theme.transition.timing.ease} opacity`};
+  transition: ${({ theme }) =>
+    `${theme.transition.duration.medium} ${theme.transition.timing.ease} opacity`};
 
   &:hover {
     opacity: 0.6;
   }
-`
+`;
 
 const LearnMoreArrow = styled(ArrowDownCircle)`
   margin-left: 14px;
   size: 20px;
-`
+`;
 
 const AboutContentContainer = styled.div<{ isDarkMode: boolean }>`
   display: flex;
@@ -217,12 +232,12 @@ const AboutContentContainer = styled.div<{ isDarkMode: boolean }>`
   width: 100%;
   background: ${({ isDarkMode }) =>
     isDarkMode
-      ? 'linear-gradient(179.82deg, rgba(0, 0, 0, 0) 0.16%, #050026 99.85%)'
-      : 'linear-gradient(179.82deg, rgba(255, 255, 255, 0) 0.16%, #eaeaea 99.85%)'};
+      ? "linear-gradient(179.82deg, rgba(0, 0, 0, 0) 0.16%, #050026 99.85%)"
+      : "linear-gradient(179.82deg, rgba(255, 255, 255, 0) 0.16%, #eaeaea 99.85%)"};
   @media screen and (min-width: ${BREAKPOINTS.md}px) {
     padding: 0 96px 5rem;
   }
-`
+`;
 
 const CardGrid = styled.div<{ cols: number }>`
   display: grid;
@@ -237,8 +252,8 @@ const CardGrid = styled.div<{ cols: number }>`
     // At this screen size, we show up to 2 columns.
     grid-template-columns: ${({ cols }) =>
       Array.from(Array(cols === 2 ? 2 : 1))
-        .map(() => '1fr')
-        .join(' ')};
+        .map(() => "1fr")
+        .join(" ")};
     gap: 32px;
   }
 
@@ -246,11 +261,11 @@ const CardGrid = styled.div<{ cols: number }>`
     // at this screen size, always show the max number of columns
     grid-template-columns: ${({ cols }) =>
       Array.from(Array(cols))
-        .map(() => '1fr')
-        .join(' ')};
+        .map(() => "1fr")
+        .join(" ")};
     gap: 32px;
   }
-`
+`;
 
 const LandingSwapContainer = styled.div`
   height: ${({ theme }) => `calc(100vh - ${theme.mobileBottomBarHeight}px)`};
@@ -259,7 +274,7 @@ const LandingSwapContainer = styled.div`
   flex-direction: column;
   align-items: center;
   z-index: 1;
-`
+`;
 
 const SwapCss = css`
   * {
@@ -268,57 +283,63 @@ const SwapCss = css`
 
   &:hover {
     transform: translateY(-4px);
-    transition: ${({ theme }) => `transform ${theme.transition.duration.medium} ${theme.transition.timing.ease}`};
+    transition: ${({ theme }) =>
+      `transform ${theme.transition.duration.medium} ${theme.transition.timing.ease}`};
   }
-`
+`;
 
 const LinkCss = css`
   text-decoration: none;
   max-width: 480px;
   width: 100%;
-`
+`;
 
 const LandingSwap = styled(Swap)`
   ${SwapCss}
   &:hover {
     border: 1px solid ${({ theme }) => theme.accentAction};
   }
-`
+`;
 
 const Link = styled(NativeLink)`
   ${LinkCss}
-`
+`;
 
 const WidgetLandingLink = styled(NativeLink)`
   ${LinkCss}
   ${SwapCss}
-`
+`;
 
 export default function Landing() {
-  const isDarkMode = useIsDarkMode()
+  const isDarkMode = useIsDarkMode();
 
-  const cardsRef = useRef<HTMLDivElement>(null)
+  const cardsRef = useRef<HTMLDivElement>(null);
 
-  const [showContent, setShowContent] = useState(false)
-  const selectedWallet = useAppSelector((state) => state.user.selectedWallet)
-  const navigate = useNavigate()
-  const location = useLocation()
+  const [showContent, setShowContent] = useState(false);
+  const selectedWallet = useAppSelector((state) => state.user.selectedWallet);
+  const navigate = useNavigate();
+  const location = useLocation();
   const queryParams = parse(location.search, {
     ignoreQueryPrefix: true,
-  })
+  });
 
-  const swapWidgetEnabled = useSwapWidgetEnabled()
+  const swapWidgetEnabled = useSwapWidgetEnabled();
+
+  const ch = useWeb3React();
+  const { infoLink, backgroundColor, color } = getChainInfoOrDefault(
+    ch.chainId
+  );
 
   // This can be simplified significantly once the flag is removed! For now being explicit is clearer.
   useEffect(() => {
     if (queryParams.intro || !selectedWallet) {
-      setShowContent(true)
+      setShowContent(true);
     } else {
-      navigate('/swap')
+      navigate("/swap");
     }
-  }, [navigate, selectedWallet, queryParams.intro])
+  }, [navigate, selectedWallet, queryParams.intro]);
 
-  const shouldDisableNFTRoutes = useAtomValue(shouldDisableNFTRoutesAtom)
+  const shouldDisableNFTRoutes = useAtomValue(shouldDisableNFTRoutesAtom);
 
   return (
     <Trace page={InterfacePageName.LANDING_PAGE} shouldLogImpression>
@@ -343,7 +364,7 @@ export default function Landing() {
           </LandingSwapContainer>
           <Gradient isDarkMode={isDarkMode} />
           <GlowContainer>
-            <Glow />
+            <Glow bgColor={color || "#ff00c7"} />
           </GlowContainer>
           <ContentContainer isDarkMode={isDarkMode}>
             <TitleText isDarkMode={isDarkMode}>
@@ -375,7 +396,7 @@ export default function Landing() {
             </ActionsContainer>
             <LearnMoreContainer
               onClick={() => {
-                cardsRef?.current?.scrollIntoView({ behavior: 'smooth' })
+                cardsRef?.current?.scrollIntoView({ behavior: "smooth" });
               }}
             >
               <Trans>Learn more</Trans>
@@ -384,17 +405,26 @@ export default function Landing() {
           </ContentContainer>
           <AboutContentContainer isDarkMode={isDarkMode}>
             <CardGrid cols={2} ref={cardsRef}>
-              {MAIN_CARDS.map(({ darkBackgroundImgSrc, lightBackgroundImgSrc, ...card }) => (
-                <Card
-                  {...card}
-                  backgroundImgSrc={isDarkMode ? darkBackgroundImgSrc : lightBackgroundImgSrc}
-                  key={card.title}
-                />
-              ))}
+              {MAIN_CARDS.map(
+                ({ darkBackgroundImgSrc, lightBackgroundImgSrc, ...card }) => (
+                  <Card
+                    {...card}
+                    backgroundImgSrc={
+                      isDarkMode ? darkBackgroundImgSrc : lightBackgroundImgSrc
+                    }
+                    key={card.title}
+                  />
+                )
+              )}
             </CardGrid>
             <CardGrid cols={3}>
               {MORE_CARDS.map(({ darkIcon, lightIcon, ...card }) => (
-                <Card {...card} icon={isDarkMode ? darkIcon : lightIcon} key={card.title} type={CardType.Secondary} />
+                <Card
+                  {...card}
+                  icon={isDarkMode ? darkIcon : lightIcon}
+                  key={card.title}
+                  type={CardType.Secondary}
+                />
               ))}
             </CardGrid>
             <ProtocolBanner />
@@ -403,5 +433,5 @@ export default function Landing() {
         </PageContainer>
       )}
     </Trace>
-  )
+  );
 }
